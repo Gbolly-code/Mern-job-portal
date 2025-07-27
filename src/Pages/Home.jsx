c0f3196bb346ee5df49bef1ac917ee10c557c6a8
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-07-27 12:28:58 +0100

    Initial commit - created a MERN app

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-blue'>
+      homepage
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
