065a1f65c8cf668a29d4292cbad5205b07fac501
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-07-27 16:10:26 +0100

    added icons from react

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { FaBarsStaggered, FaXmark } from 'react-icons/fa6'
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -13,10 +14,10 @@ const Navbar = () => {
         {path: '/salary', title: 'Salary Estimate'},
         {path: '/post-job', title: 'Post A Job'},
     ]
-    console.log(navItems)
+    
   return (
-    <header>
-        <nav>
+    <header className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>
+        <nav className='flex justify-between items-center py-6'>
             <a href="/" className='flex items-center gap-2 text-2xl text-black'>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -32,11 +33,11 @@ const Navbar = () => {
                     fillOpacity="0.4"/>
                     <circle cx="16.9857" cy="17.4857" r="12.0143" fill="#3575E2" />
                     </svg>{" "}
-                    <span>Job Portal</span></a>
+                    <span>JobPortal</span></a>
 
                     {/* nav items for large devices */}
 
-                    <ul className='flex gap-12'>
+                    <ul className='hidden md:flex flex gap-12'>
                            {navItems.map(({ path, title }) => (
                                 <li key={path} className='text-base text-primary'>
                                     <NavLink
@@ -50,9 +51,19 @@ const Navbar = () => {
                             ))}
                     </ul>
 
-                    <div className='text:base text:primary font-medium space-x-5 hidden lg:block'>
-                       <Link to='/login'>Log in</Link>
-                       <Link to='/sign-up'>Sign Up</Link>
+                    <div className='text-base text-primary font-medium space-x-5 hidden lg:block'>
+                       <Link to='/login' className='py-2 px-5 border rounded'>Log in</Link>
+                       <Link to='/sign-up' className='py-2 px-5 border rounded bg-blue text-white'>Sign Up</Link>
+                    </div>
+
+
+                    {/*mobile menu*/}
+                    <div className='md:hidden block'>
+                        <button onClick={handleMenuToggler}>
+                           {
+                            isMenuOpen ? <FaXmark className='w-5 h-5 text-primary' /> : <FaBarsStaggered className='w-5 h-5 text-primary'/>
+                           }
+                        </button>
                     </div>
         </nav>
     </header>
