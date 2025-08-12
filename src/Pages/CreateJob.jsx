eae5dc002a261c2ad560ef11d294936e82b7dbc5
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-08-12 16:34:57 +0100

    added salary range

--- src/Pages/CreateJob.jsx
+++ src/Pages/CreateJob.jsx
@@ -15,9 +15,36 @@ const CreateJob = () => {
       {/* form */}
       <div className='bg-[#FAFAFA] py-10 px-4 lg:px-16'>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div></div>
+
+            {/* first row */}
+            <div className='create-job-flex'>
+             <div className='lg:w-1/2 w-full'>
+               <label className='block mb-2 text-lg'>Job Title</label>
+                <input type="text" defaultValue={"Web Developer"} {...register("jobTitle")} 
+                className='create-job-input'/>
+             </div>
+             <div className='lg:w-1/2 w-full'>
+               <label className='block mb-2 text-lg'>Company Name</label>
+                <input type="text" placeholder="Ex: Microsoft" {...register("companyName")} 
+                className='create-job-input'/>
+             </div>
+            </div>
+
+            {/* second row */}
+            <div className='create-job-flex'>
+             <div className='lg:w-1/2 w-full'>
+               <label className='block mb-2 text-lg'>Minimum Salary</label>
+                <input type="text" placeholder="$20k" {...register("minPrice")} 
+                className='create-job-input'/>
+             </div>
+             <div className='lg:w-1/2 w-full'>
+               <label className='block mb-2 text-lg'>Maximum Salary</label>
+                <input type="text" placeholder="$120k" {...register("maxPrice")} 
+                className='create-job-input'/>
+             </div>
+            </div>
               
-            <input type="submit" />
+            <input type="submit" className='my-5' />
         </form>
 
       </div>
